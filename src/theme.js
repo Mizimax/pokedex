661386710a00eeaa808260d490dd862cae923d42
tammanoonjomjaturong <max.tammanoon@skooldio.com>
2022-05-04 18:28:42 +0700

    💄 Add base color theme

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,28 @@
-const theme = {};
+const ELEMENT_COLORS = {
+  Psychic: '#f8a5c2',
+  Fighting: '#f0932b',
+  Fairy: '#c44569',
+  Normal: '#f6e58d',
+  Grass: '#badc58',
+  Metal: '#95afc0',
+  Water: '#3dc1d3',
+  Lightning: '#f9ca24',
+  Darkness: '#574b90',
+  Colorless: '#FFF',
+  Fire: '#eb4d4b',
+};
+
+const COLORS = {
+  primary: {
+    300: '#dc7777',
+    500: '#ec5656',
+  },
+  secondary: '#ffffff',
+};
+
+const theme = {
+  color: COLORS,
+  elements: ELEMENT_COLORS,
+};
 
 export default theme;
