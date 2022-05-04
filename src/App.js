7bd45818d872a597dce6b7bbff63f15a111f8451
tammanoonjomjaturong <max.tammanoon@skooldio.com>
2022-05-04 18:15:50 +0700

    🎨  Structure theme and route

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-import React, { Component } from 'react'
-import './App.css'
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
-const COLORS = {
-  Psychic: "#f8a5c2",
-  Fighting: "#f0932b",
-  Fairy: "#c44569",
-  Normal: "#f6e58d",
-  Grass: "#badc58",
-  Metal: "#95afc0",
-  Water: "#3dc1d3",
-  Lightning: "#f9ca24",
-  Darkness: "#574b90",
-  Colorless: "#FFF",
-  Fire: "#eb4d4b"
+import { default as PokedexRoute } from './Routes/Pokedex';
+import theme from './theme';
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <PokedexRoute />
+    </ThemeProvider>
+  );
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    )
-  }
-}
-
-export default App
+export default App;
